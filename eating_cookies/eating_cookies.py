ea2f06ce0ad6d3a0a5d4e925bd8b70cb2bd4a468
D-Fink <dfink4badjuju@gmail.com>
2020-04-16 21:44:27 -0600

    3/5

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,19 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  #if negative return 0
+  if cache is None:
+    cache = [0] * (n + 1)
+  if n <= 1:
+    cache[n] = 1
+    return 1
+  elif n == 2:
+    cache[n] = 2
+    return 2
+  elif cache[n] == 0:
+    eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+  return cache[n]
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,23 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass
+    batch_by_ingred = []
+    batches = 0
+    # if ingredients is missing part of recipe return 0
+    if len(recipe) != len(ingredients):
+      return batches
+    else:
+      for i in recipe:
+        #push amount of batches per ingredients to list
+          batch_by_ingred.append(ingredients[i] // recipe[i])
+          #find the minimum number of batches in list
+      batches = min(batch_by_ingred)
+      return batches
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }
+  ingredients = { 'milk': 1288, 'flour': 9, 'sugar': 95 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
